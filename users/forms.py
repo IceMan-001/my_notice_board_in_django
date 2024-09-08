ad9b44c89b87ea03640d24a52f068733dd246006
Andrey Bobrov <8867006@mail.ru>
2024-09-08 16:02:03 +0300

    add bootstrap

---
+++ users/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+# Получение текущей модели пользователя
+User = get_user_model()
+
+
+# Создание формы регистрации пользователя
+class UserRegistrationForm(forms.ModelForm):
+    # Создание дополнительного поля пароля для повторного ввода при регистрации
+    password2 = forms.CharField(label='Повторите пароль',
+                                widget=forms.PasswordInput)  # widget=forms.PasswordInput - скрытый ввод
+
+    def clean_password2(self):
+        cleaned_data = self.cleaned_data
+        if cleaned_data['password'] != cleaned_data['password2']:
+            raise forms.ValidationError('Пароли не совпадают!')  # Если пароли не совпадают - генерируем исключение
+        return cleaned_data['password2']
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'first_name', 'last_name', 'email', 'phone', 'city')
+
+
+class CustomPasswordChangeForm(forms.Form):
+    old_password = forms.CharField(label="Старый пароль", widget=forms.PasswordInput)
+    new_password_1 = forms.CharField(label="Новый пароль", widget=forms.PasswordInput)
+    new_password_2 = forms.CharField(label="Повторите новый пароль", widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        new_password_1 = cleaned_data['new_password_1']
+        new_password_2 = cleaned_data['new_password_2']
+        old_password = cleaned_data['old_password']
+
+        if new_password_1 and new_password_2 and new_password_1 != new_password_2:
+            raise forms.ValidationError('Пароли не совпадают')
+
+        if new_password_1 and new_password_2 and new_password_1 == new_password_2 and new_password_1 == old_password:
+            raise forms.ValidationError("Пароли совпадают с текущим")
+
+        return cleaned_data
--- users/views.py
+++ users/views.py
@@ -1,8 +1,37 @@
-from django.shortcuts import render
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
+
+from django.contrib.auth import update_session_auth_hash
+from my_board.settings import LOGIN_REDIRECT_URL
+from .forms import UserRegistrationForm, CustomPasswordChangeForm
+
+User = get_user_model()
 
 
 def register(request):
-    pass
+    # если нажали кнопку регистрации (метод POST)
+    if request.method == 'POST':
+        # создаем объект с данными из запроса
+        user_form = UserRegistrationForm(request.POST)
+        # Валидация формы (правильность введенных данных)
+        if user_form.is_valid():
+            # Создание объекта с полями формы (без сохранения в БД)
+            new_user = user_form.save(commit=False)
+            # Хеширование пароля пользователя
+            new_user.set_password(user_form.cleaned_data['password'])  # Хеширование пароля
+            # Сохранение пользователя в БД
+            new_user.save()  # по умолчанию commit=True
+            context = {'title': 'Успешная регистрация', 'new_user': new_user}
+            return render(request, template_name='users/register_done.html', context=context)
+
+    # Если метод GET - отрисовка страницы регистрации
+    user_form = UserRegistrationForm()
+    context = {'title': 'Регистрация', 'register_form': user_form}
+    return render(request, template_name='users/register.html', context=context)
 
 
 def log_in(request):
